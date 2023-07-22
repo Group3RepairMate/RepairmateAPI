6d3723bba841591a8b3d65ce3dab81ac97615a0e
harshil3662 <vaghaniharshil11291@gmail.com>
2023-07-22 00:50:14 -0400

    Add existing project files to Git

--- RepairmateApp.js
+++ RepairmateApp.js
@@ -1,4 +1,43 @@
 var express = require('express')
+const mongoose = require('mongoose')
+const mongoString = "mongodb+srv://repairmateadmin:3662@repairmate.dy9r1vc.mongodb.net/repairmates"
+
+mongoose.connect(mongoString);
+const db = mongoose.connection;
+
+db.on('error', (error) => {
+   console.log(error)
+})
+
+db.once('connected', () => {
+   console.log('Database Connected');
+})
+
+const mechanic = new mongoose.Schema({
+   name: {
+     type: String,
+     required: true,
+   },
+   email: {
+     type: String,
+     required: true,
+   },
+   phone_no: {
+     type: String,
+     required: true,
+   },
+   location: {
+      type: String,
+      required: true,
+    },
+    availability: {
+     type: String,
+     required: true,
+   }
+ })
+
+ const mechanicModel = mongoose.model('Mechanics', mechanic);
+
 var app = express()
 var fs = require("fs")
 var bodyParser = require('body-parser')
@@ -7,53 +46,27 @@ app.use(bodyParser.urlencoded({
   extended: true
 })); 
 
-var user = {
-       "name" : "A one service",
-       "email" : "aone@gmail.com",
-       "phone_no" : "6578909876",
-       "location" : "160,Canon Jackson Dr",
-       "availability" : "both",
-       "since" : "20 years"
-}
-
-
 app.get("/garages", function (req, res) {
-   fs.readFile("garages.json", 'utf8', function (err, data) {
-      data = JSON.parse(data)
-      for(let i=0;i<data["list"].length;i++){
-         if(data["list"].length==1){
-            if(data["list"][i]===null){
-               data["list"]=[]
-            }
-         }
-      }
-      fs.writeFile("garages.json",JSON.stringify(data),function(err){})
-      res.end( JSON.stringify(data) )
+   mechanicModel.find({}).then(data => {
+      const garages = {list:data}
+      res.json(garages)
+   }).catch(error => {
+      console.error(error)
+      res.status(500).json({ error: 'Internal server error' });
    })
 })
 
-app.post("/addGarage", function (req, res) {
-    fs.readFile("garages.json", 'utf8', function (err, data) {
-      data = JSON.parse(data)
-       model = req.body
-       model["id"] = data["list"].length + 1
-       data["list"].push(model)
-       fs.writeFile("garages.json",JSON.stringify(data),function(err){})
-       res.redirect("/garages")
-    })
- })
 
- app.get('/deleteGarage/:id', function (req, res) {
-    fs.readFile("garages.json", 'utf8', function (err, data) {
-       data = JSON.parse( data )
-       for(let i=0;i<data["list"].length;i++){
-         if(data["list"][i]["id"]==req.params.id){
-            delete data["list"][i]
-         }
-       }
-       fs.writeFile("garages.json",JSON.stringify(data),function(err){})
-       res.redirect("/garages")
-    })
+app.post("/addGarage", async function (req, res) {
+   const newMechanic = new mechanicModel({
+      name : req.body["name"],
+      email : req.body["email"],
+      phone_no : req.body["phone_no"],
+      location : req.body["location"],
+      availability : req.body["availability"]
+   })
+   newMechanic.save()
+   res.redirect("/garages")
  })
 
 app.listen(8080, function () {
