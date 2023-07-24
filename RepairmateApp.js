2a1efd03219ec75f94e558d785e5995b72605a72
harshil3662 <vaghaniharshil11291@gmail.com>
2023-07-23 21:02:04 -0400

    add api endpoint

--- RepairmateApp.js
+++ RepairmateApp.js
@@ -2,15 +2,15 @@ var express = require('express')
 const mongoose = require('mongoose')
 const mongoString = "mongodb+srv://repairmateadmin:3662@repairmate.dy9r1vc.mongodb.net/repairmates"
 
-mongoose.connect(mongoString);
-const db = mongoose.connection;
+mongoose.connect(mongoString)
+const db = mongoose.connection
 
 db.on('error', (error) => {
    console.log(error)
 })
 
 db.once('connected', () => {
-   console.log('Database Connected');
+   console.log('Database Connected')
 })
 
 const mechanic = new mongoose.Schema({
@@ -36,38 +36,53 @@ const mechanic = new mongoose.Schema({
    }
  })
 
- const mechanicModel = mongoose.model('Mechanics', mechanic);
+const mechanicModel = mongoose.model('Mechanics', mechanic)
 
 var app = express()
 var fs = require("fs")
 var bodyParser = require('body-parser')
-app.use( bodyParser.json() );       
+app.use( bodyParser.json() )       
 app.use(bodyParser.urlencoded({     
   extended: true
-})); 
+}))
 
-app.get("/garages", function (req, res) {
-   mechanicModel.find({}).then(data => {
-      const garages = {list:data}
-      res.json(garages)
-   }).catch(error => {
-      console.error(error)
-      res.status(500).json({ error: 'Internal server error' });
-   })
+app.get("/garages", async function (req, res) {
+   var data = await mechanicModel.find({})
+   const garages = {list:data}
+   res.json(garages)
 })
 
 
 app.post("/addGarage", async function (req, res) {
-   const newMechanic = new mechanicModel({
+   await mechanicModel.create({
       name : req.body["name"],
       email : req.body["email"],
       phone_no : req.body["phone_no"],
       location : req.body["location"],
       availability : req.body["availability"]
    })
-   newMechanic.save()
    res.redirect("/garages")
  })
+
+ app.post("/edit", async function (req, res) {
+   try {
+      const updatedMechanic = await mechanicModel.updateOne(
+         { email: req.body["oldEmail"] },
+         {
+            name: req.body["name"],
+            email: req.body["email"],
+            phone_no: req.body["phone_no"],
+            location: req.body["location"]
+         }
+      )
+
+      res.redirect("/garages");
+   } catch (error) {
+      console.error("Error while updating mechanic:", error)
+      res.status(500).send("Internal Server Error")
+   }
+});
+
 
 app.listen(8080, function () {
 })
